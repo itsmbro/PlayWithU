91959e433c825f0cf538304a5d10f46bc58bfa7f
itsmbro <belottimichele21@gmail.com>
2025-06-24 16:20:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,60 +11,63 @@ Divertitevi a scoprire nuove sensazioni insieme!
 **NB:** Consenso e rispetto sempre al primo posto.
 """)
 
+# Piaceri originali PER LEI (come forniti da te)
 sfide_per_lei = [
-    "Legami piedi e le mani: leccami SOLO DAVANTI PER 5 minuti",
-    "Legami piedi e mani: leccami solo dietro per 3 minuti e le mani le usi davanti",
-    "Mi devi legare le mani, mettere a pecora e leccarla con un dito dietro. Vai avanti per 5 minuti. In 5 minuti puoi leccarla solo un minuto",
-    "Mi fai stendere a pancia in alto (a stella), mi schiacci la pancia, mi bendi gli occhi e inizi a toccarmi piano, tipo massaggio.. dopo un po’ con una mano continui e con l’altra un dito dietro",
-    "Mi bendi gli occhi e mi tappi la bocca, mi morsichi il collo, le tette, la pancia",
-    "Mi leghi i piedi, li metti alla tua spalla, inizi a leccarli e a morderli",
-    "Devi guardare un massaggio sui porno, prendere su punto e provare a farlo su di me",
-    "Devo riempirti di olio, massaggiare e poi guardare mentre mi tocco da sola per 5 minuti",
-    "Devi mettere la panna montata sul culo, se vuoi anche le fragole, e mangiarle direttamente da lì",
-    "Con un cubetto di ghiaccio in bocca, parti dalla bocca e arrivi ai piedi",
-    # Aggiunte mie:
-    "Ti leghi i polsi dietro la schiena e ti muovi solo col bacino per 10 minuti",
-    "Ti massaggio i piedi con olio caldo mentre mi guardi senza poter toccarmi",
-    "Mi fai baciare ogni dito dei tuoi piedi lentamente uno per uno",
-    "Mi metti a pecora e mi fai un massaggio lento e profondo sulla schiena e le chiappe",
-    "Usi il tuo corpo come una frusta, sfregandoti su di me con controllo e passione",
-    "Ti lego le mani sopra la testa e ti coccolo i piedi per 7 minuti senza fermarti",
-    "Indossi le calze più sensuali che hai e mi comandi di baciartele e leccartele per 5 minuti",
-    "Mi tocchi e mi guardi mentre ti rilasso le gambe, poi passi al bacino con lentezza",
-    "Ti impongo di non fare rumore mentre ti tocco ogni centimetro del corpo",
-    "Ti lecco piano la schiena, scendendo verso il sedere e ti costringo a trattenere il respiro"
+    "Legami piedi e le mani : leccami SOLO DAVANTI PER 5 minuti",
+    "⁠legami piedi e mani: leccami solo dietro per 3 minuti e le mani le usi davanti",
+    "⁠ mi devi legare le mani, mettere a pecora e leccarla con un dito dietro. Vai avanti per 5 minuti. In 5 minuti puoi leccarla solo un minuto",
+    "⁠mi fai straniare a pancia in altro (a stella) mi schiacci la pancia, mi bendi gli occhi e inizi a toccarmi, Piano, tipo un massaggio.. dopo un po’ con una mano continui e con l’altra un dito dietro",
+    "⁠mi bendi gli occhi e mi tappi la bocca, mi morsichi il collo, le tette, la pancia",
+    "⁠mi leghi i piedi, li metti alla tua spalle, inizi a leccarli e a morderli",
+    "⁠devi guardare un massaggio sui porno, prendere su punto e provare a farlo su di me",
+    "⁠devo riempiri di olio, massaggiare e poi guardare mentre mi tocco da sola per 5 minuti",
+    "⁠devi mettere la panna montata sul culo, se vuoi anche le fragole, e mangiate direttamente da lì",
+    "⁠con un cubetto di ghiaccio in bocca, parti dalla bocca e arrivi ai piedi"
 ]
 
+# Nuove sfide originali PER LEI, ispirate ai suoi piaceri (10)
+sfide_per_lei += [
+    "Ti lego le mani sopra la testa e ti massaggio lentamente le cosce fino al bacino per 10 minuti",
+    "Con le mani legate, devi ascoltare solo la mia voce mentre ti guido in un gioco sensoriale",
+    "Ti massaggio i piedi con olio caldo mentre sei bendata e non puoi muoverti",
+    "Mi costringi a leccarti ogni dito dei piedi uno per uno, con calma e attenzione",
+    "Mi metti a pecora e con le dita accarezzi la schiena e le chiappe fino a farmi rabbrividire",
+    "Usi il tuo corpo come una frusta, sfregandoti su di me con controllo e passione per 7 minuti",
+    "Indossi le calze più sensuali che hai e mi ordini di baciartele e leccartele senza mai fermarmi",
+    "Mi obblighi a non fare rumore mentre ti tocco ogni centimetro del corpo",
+    "Ti lecco lentamente la schiena scendendo verso il sedere e ti costringo a trattenere il respiro",
+    "Mi fai un massaggio lento e profondo con olio caldo, mentre mi bendi e non posso vedere nulla"
+]
+
+# Piaceri originali PER LUI (come forniti da te)
 sfide_per_lui = [
-    "Devi legarmi i polsi, farmi una sega mentre mi metti i piedi al collo e in faccia e li muovi, ma non mi fai venire mai.",
-    "Devi letteralmente sederti sulla mia faccia e cercare di soffocarmi, io se non respiro più batto la mano sulla tua gamba",
-    "Devi essere aggressiva e farmi male, tirandomi i capelli, mettendomi le dita in bocca, e comandando la mia testa dai capelli tirati, per leccartela.",
-    "Seduti uno di fronte all'altro, tu ti tocchi, io pure, ma non posso venire, e giochi coi piedi per catturare la mia attenzione, facendomeli baciare ogni tanto.",
-    "Tu a pancia in giù, mi fai letteralmente impazzire muovendo il culo e stringendomelo dalle chiappe, e mi fai venire così",
-    "Mi bendi, usi varie parti del corpo da avvicinare e sfiorarmi la faccia, mentre mi fai una sega. Se non indovino smetti di farmela e vai avanti.",
-    "Devi obbligarmi a farmi mangiare del cibo dal tuo corpo, sulla vagina bagnata soprattutto, devo leccare via tutto.",
-    "Mi leghi il collo, e mi tiri dove vuoi: in mezzo alle tue gambe, sui piedi, sul culo",
-    "Mi scopi tu, e quando sto per venire, ti fermi per 10 volte. Devi vedermi il cazzo esplodere",
-    "Devi letteralmente strusciarmi la vagina in faccia, non 69 ma dal davanti, devi bagnarmi completamente la faccia, e spesso soffocarmi. Io non posso toccarmi",
-    # Aggiunte mie:
-    "Mi bendi gli occhi e mi fai toccare solo con i piedi, non posso usare le mani",
-    "Devo massaggiare il tuo corpo con olio caldo mentre tu mi dici cosa fare",
-    "Devo sedermi tra le tue gambe e farmi comandare usando solo il corpo, senza parlare",
-    "Mi metti a pecora e inizi a mordermi il collo mentre con le mani giochi con i miei piedi",
+    "devi legarmi i polsi, farmi una sega mentre mi metti i piedi al collo e in faccia e li muovi, ma non mi fai venire mai.",
+    "devi letteralmente sederti sulla mia faccia e cercare di soffocarmi, io se non respiro piu batto la mano sulla tua gamba",
+    "devi essere aggressiva e farmi male, tirandomi i capelli, mettendomile dita in bocca, e comandando la mia testa dai capelli tirati, per leccartela.",
+    "seduti uno di fronte all'altro, tu ti tocchi, io pure, ma non posso venire, e giochi coi piedi per catturare la mia attenzione, facendomeli baciare ogni tanto.",
+    "tu a pancia in giu, mi fai letteralmente impazzire muovendo il culo e stringendomelo dalle chiappe, e mi fai venire così",
+    "mi bendi, usi varie parti del corpo da avvicinare e sfiorarmi la faccia, mentre mi fai una sega. se non indovino smetti di farmela e vai avanti.",
+    "devi obbligarmi a farmi mangiare del cibo dal tuo corpo, sulla vagina bagnata soprattutto, devo leccare via tutto.",
+    "mi leghi il collo, e mi tiri dove vuoi: in mezzo alle tue gambe, sui piedi, sul culo",
+    "mi scopi tu, e quando sto per venire, ti fermi per 10 volte. devi vedermi il cazzo esplodere",
+    "devi letteralmente strusciarmi la vagina in faccia, non 69 ma dal davanti, devi bagnarmi completamente la faccia, e spesso soffocarmi. io non posso toccarmi"
+]
+
+# Nuove sfide originali PER LUI, ispirate ai suoi piaceri (10)
+sfide_per_lui += [
+    "Mi bendi gli occhi e mi fai toccare solo con i piedi, proibendomi di usare le mani per 5 minuti",
+    "Devo massaggiare il tuo corpo con olio caldo mentre mi dai ordini precisi su cosa fare",
+    "Mi metti a pecora e inizi a mordermi il collo mentre giochi con i miei piedi con le mani",
     "Mi ordini di non venire mai per 10 minuti mentre mi giochi con le chiappe e il culo",
-    "Mi fai sedere con la faccia tra le tue gambe e mi comandi di leccarti senza fermarmi",
+    "Mi fai sedere con la faccia tra le tue gambe e mi comandi di leccarti senza mai fermarmi",
     "Mi costringi a toccarmi ma non a venire, mentre tu giochi con i piedi sulla mia faccia",
     "Devo baciare lentamente ogni centimetro delle tue gambe fino a risalire al bacino",
-    "Mi fai massaggiare i tuoi piedi, poi mi metti la testa sotto di te per un’ora"
+    "Mi fai massaggiare i tuoi piedi, poi mi metti la testa sotto di te per un’ora",
+    "Mi trascini per la stanza legato solo con una sciarpa, mentre io devo obbedire ai tuoi comandi",
+    "Devo supplicarti di lasciarmi andare mentre mi tieni fermo con mani e piedi legati"
 ]
 
-def nuova_sfida(chi: str):
-    if chi == "lei":
-        return random.choice(sfide_per_lei)
-    else:
-        return random.choice(sfide_per_lui)
-
-# Memorizza le sfide già usate per evitare ripetizioni
+# Funzioni per pescare sfide senza ripetizioni
 if "sfide_lei_giocate" not in st.session_state:
     st.session_state.sfide_lei_giocate = set()
 if "sfide_lui_giocate" not in st.session_state:
@@ -75,7 +78,6 @@ def pesca_sfida(chi: str):
     giocate = st.session_state.sfide_lei_giocate if chi == "lei" else st.session_state.sfide_lui_giocate
 
     if len(giocate) == len(lista):
-        # Se abbiamo finito tutte le sfide, resettiamo
         giocate.clear()
 
     disponibili = [i for i in range(len(lista)) if i not in giocate]
